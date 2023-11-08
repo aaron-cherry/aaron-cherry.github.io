68367db1c897928d56d804dade54738e4c964ff6
aaron-cherry <106337382+aaron-cherry@users.noreply.github.com>
2023-11-07 23:04:42 -0600

    Added sequential numbering to test button output

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const testButton = document.getElementById("testButton");
+const testNumber = 0;
 
 const clickTest = () => {
+    testNumber += 1;
     let buttonDiv = document.getElementById("buttonDiv");
     let p = document.createElement("p");
-    p.textContent = "Test";
+    p.textContent = "Test" + testNumber.toString;
     buttonDiv.appendChild(p);
 }
 testButton.onclick = () => {
